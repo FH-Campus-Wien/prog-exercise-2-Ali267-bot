946eb193c7f8cacb43e90341d5dd56ce6c4e293a
ali m <alischeich67@gmail.com>
2023-11-13 16:24:43 +0100

    Done, rhombus prints the ŕight thing but does not pass the tests

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,32 +1,38 @@
 package at.ac.fhcampuswien;
 
+import com.sun.tools.jdeprscan.scan.Scan;
+import org.graalvm.compiler.virtual.phases.ea.PartialEscapeClosure;
+
 import java.util.Scanner;
 
 public class App {
 
     //todo Task 1
     public void largestNumber(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Number 1: ");
-        double eingabe = scanner.nextDouble();
 
         int counter = 1;
-        double number = eingabe;
+        float number, largestNum = 0;
 
-        if (eingabe <= 0 && counter == 1){
-            System.out.println("No number entered.");
-        }else {
-            while(eingabe > 0){
-                counter++;
-                System.out.println("Number" + counter + ": ");
-                eingabe = scanner.nextDouble();
+        Scanner scanner = new Scanner(System.in);
 
-                if (eingabe > number)
-                    number = eingabe;
+        do {
+            System.out.print("Number " + counter + ": ");
+            number = scanner.nextFloat();
+
+            if (counter == 1 && number <= 0){
+                System.out.println("No number entered.");
+                return;
             }
-            System.out.print("The largest number is " + String.format("%.2f", number));
+            if (number > largestNum){
+                largestNum = number;
+            }
+            counter++;
+            if (number <= 0){
+                System.out.println("The largest number is " + String.format("%.2f", largestNum));
+                return;
+            }
 
-        }
+        }while (true);
 
 
         }
@@ -36,37 +42,35 @@ public class App {
 
     //todo Task 2
     public void stairs() {
-        Scanner scanner2 = new Scanner(System.in);
-        int n = scanner2.nextInt();
-        System.out.print("n: ");
-        int i, j, number = 1;
+        Scanner scanner = new Scanner(System.in);
+        int input = 1;
 
-        if (n < 0) {
+        System.out.print("n: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0){
             System.out.println("Invalid number!");
         }
-
-        for (i = 1; i <= n; i = i + 1) {
-            for (j = 1; j <= i; j++) {
-                System.out.print(number + " ");
-                number = number + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(input++ + " ");
             }
             System.out.println();
         }
+
     }
 
 
 
     //todo Task 3
     public void printPyramid(){
-        int i;
-        int j;
-         int rowsNum = 6;
+        final int rows = 6;
 
-        for (i = 0; i <= rowsNum; i++){
-            for (j = rowsNum - i - 1; j > 0;j--) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 0; j < rows - i; j++) {
                 System.out.print(" ");
             }
-            for (j = 0; j < i * 2 +1;j++) {
+            for (int j = 0; j < i * 2 - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -76,37 +80,109 @@ public class App {
     //todo Task 4
     public void printRhombus(){
         // input your solution here
+        System.out.print("h: ");
+        int h = new java.util.Scanner(System.in).nextInt();
+
+        System.out.print("c: ");
+        char c = new java.util.Scanner(System.in).next().charAt(0);
+
+        int colsVar = h - 1, charIteratorUpper = 0, charIteratorLower= 0;
+
+        if (h % 2 == 0){
+            System.out.println("Invalid number!");
+            return;
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < Math.abs((h-1)/2 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < h - Math.abs(colsVar); j++) {
+                if (i < ((h + 1) / 2)){
+                    System.out.print((char) (c - Math.abs(charIteratorUpper + j)));
+                }else System.out.print((char) (c - Math.abs((h-1)/2 - 1 - charIteratorLower - j)));
+            }
+            colsVar = colsVar - 2; //for the printing of the characters
+            System.out.println();
+            if ((i < ((h + 1) / 2))) {
+                charIteratorUpper--;
+            } else {
+                charIteratorLower++;
+            }
+        }
     }
 
     //todo Task 5
     public void marks(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+
+        int mark = 1, negativeMarks = 0, input;
+        double average = 0, sum=0;
+
+        do {
+            System.out.print("Mark " + mark + ": ");
+            input = scanner.nextInt();
+
+            while (input < 0 || input > 5){
+                System.out.println("Invalid mark!");
+                System.out.print("Mark " + mark + ": ");
+                input = scanner.nextInt();
+            }
+
+            sum += input;
+
+            if (input == 0){
+                if (mark == 1){
+                    average = sum / mark;
+                }else average = sum / (mark - 1);
+            }
+
+            if (input == 5){
+                negativeMarks++;
+            }
+
+            mark++;
+
+            if (input == 0){
+                System.out.println("Average: " + String.format("%.2f",average));
+                System.out.println("Negative marks: " + negativeMarks);
+            }
+
+        }while (input != 0);
+
     }
 
     //todo Task 6
     public int happyNumbers() {
-        Scanner scanner3 = new Scanner(System.in);
+        int n, digit, sum = 0;
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("n: ");
-        int n = scanner3.nextInt();
-        int happy = n;
-        int sum = 0;
-        int digit;
+        n = scanner.nextInt();
 
+        if (n <= 0){
+            System.out.println("Number invalid");
+            return 0;
+        }
 
-        while (n != 1 & n != 4) {
-            while (n != 0) {
+        while (true) {
+            while (n > 0) {
                 digit = n % 10;
-                sum += Math.pow(digit, 2);
-                n = n / 10;
+                sum = sum + digit * digit;
+                n /= 10;
             }
             n = sum;
             sum = 0;
+            if (n == 1) {
+                System.out.println("Happy number!");
+                break;
+            } else if (n == 4) {
+                System.out.println("Sad number!");
+                break;
+            }
         }
-        if (n == 1) {
-            System.out.println("Happy number!");
-        } else
-            System.out.println("Sad number!");
-        return sum;
+        return 0;
     }
 
 
